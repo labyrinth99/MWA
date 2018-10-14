a4c8caa2e916e46560722f63f025c75a566bfc88
labyrinth81 <41203957+labyrinth81@users.noreply.github.com>
2018-10-14 13:32:44 -0500

    Assignment 10

---
+++ Assignment10/exercise_03.ts
@@ -0,0 +1,13 @@
+class Person {
+    private _firstName: string;
+    get fname() {
+        return this._firstName;
+    }
+    set fname(fname: string) {
+        this._firstName = fname;
+    }
+}
+
+let p = new Person();
+p.fname = "Asaad";
+console.log(p.fname);
